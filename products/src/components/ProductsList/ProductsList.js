b035534abef7c76d739a0c025d59c61623e3110e
Elena Yaneva <elena_yaneva_@abv.bg>
2023-08-11 20:40:32 +0300

    sorting, rating

--- products/src/components/ProductsList/ProductsList.js
+++ products/src/components/ProductsList/ProductsList.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 export const ProductsList = () => {
     const [data, setData] = useState([]);
+    const [sortedData, setSortedData] = useState([]);
     const location = useLocation();
 
     let category = 'all';
@@ -25,6 +26,27 @@ export const ProductsList = () => {
         categoryName = 'ALL PRODUCTS';
     }
 
+    const [sortBy, setSortBy] = useState('nameAsc');
+
+    const handleSortChange = (event) => {
+        setSortBy(event.target.value);
+    };
+
+    const renderStars = (rating) => {
+        const maxRating = 5;
+        const starArray = [];
+
+        for (let i = 0; i < maxRating; i++) {
+            if (i < rating) {
+                starArray.push(<span key={i} className="star filled"><i className="fas fa-star"></i></span>);
+            } else {
+                starArray.push(<span key={i} className="star"><i className="far fa-star"></i></span>);
+            }
+        }
+
+        return starArray;
+    };
+
     useEffect(() => {
         fetch('data.json')
             .then(response => response.json())
@@ -38,6 +60,22 @@ export const ProductsList = () => {
             });
     }, [category]);
 
+    useEffect(() => {
+        let sortedDataCopy = [...data];
+
+        if (sortBy === 'nameAsc') {
+            sortedDataCopy.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (sortBy === 'nameDesc') {
+            sortedDataCopy.sort((a, b) => b.name.localeCompare(a.name));
+        } else if (sortBy === 'priceAsc') {
+            sortedDataCopy.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+        } else if (sortBy === 'priceDesc') {
+            sortedDataCopy.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+        }
+
+        setSortedData(sortedDataCopy);
+    }, [data, sortBy]);
+
     return (
         <section className="main-container">
             <section className="filter">
@@ -45,17 +83,25 @@ export const ProductsList = () => {
             </section>
 
             <section className="productsList">
-
                 <section className="small-container">
                     <div className="categoryName">
-                        <h3>{categoryName}</h3>
-                        <h5>{data.length} products</h5>
+                        <h2>{categoryName}</h2>
+                        <h5>{sortedData.length} products</h5>
                     </div>
-                    <h3 className="sort">SORT BY:</h3>
+
+                    <div className="sort-container">
+                        <h4 className="sort">SORT BY:</h4>
+                        <select onChange={handleSortChange} value={sortBy}>
+                            <option value="nameAsc">Name A-Z</option>
+                            <option value="nameDesc">Name Z-A</option>
+                            <option value="priceAsc">Price Low-High</option>
+                            <option value="priceDesc">Price High-Low</option>
+                        </select>
+                    </div>
                 </section>
 
                 <section className="grid">
-                    {data.map(x =>
+                    {sortedData.map(x =>
                         <div key={x.id} className="grid-item">
                             <img src={x.img} alt="img" />
                             <section className="moreInfo">
@@ -63,13 +109,14 @@ export const ProductsList = () => {
                                 <p>{x.description}</p>
                                 <h4>{parseFloat(x.price).toFixed(2)} euro</h4>
                                 <h4>{x.discountPrice}</h4>
-                                <h4>{x.rating}</h4>
+                                <div className="rating">
+                                    {renderStars(x.rating)}
+                                </div>
                                 <button>Add to Cart</button>
                             </section>
                         </div>
                     )}
                 </section>
-
             </section>
 
             <button className="btn">Load More</button>
